1bc326146595ae6f905af10b138774428b244999
MJavad Hasanzade <hjavad522@gmail.com>
2022-03-30 19:55:00 +0430

    feat: add create ingredient button

--- pages/ingredients.tsx
+++ pages/ingredients.tsx
@@ -8,7 +8,6 @@ import Head from "next/head";
 import Link from "next/link";
 
 const Ingredients: NextPage = ({ ingredients }) => {
-  console.log(ingredients);
 
   return (
     <>
@@ -17,7 +16,7 @@ const Ingredients: NextPage = ({ ingredients }) => {
         <Title>Ingredients</Title>
 
         <div className="actions my-4">
-          <Link href="/ingredient/add" passHref>
+          <Link href="/ingredients/add" passHref>
             <Button type="secondary">Add Ingredient</Button>
           </Link>
         </div>
@@ -36,7 +35,7 @@ export default Ingredients;
 
 export async function getServerSideProps(context) {
   const ingredientRes = await fetch(
-    process.env.API_URL + "ingredients" || "http://localhost:5000"
+    process.env.NEXT_PUBLIC_API_URL + "ingredients" || "http://localhost:5000"
   );
   const ingredients = await ingredientRes.json();
 
