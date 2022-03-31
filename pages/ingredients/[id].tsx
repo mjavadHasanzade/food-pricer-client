c2aa1ad2a9383356ee484c8a0d25d65ccca357a9
MJavad Hasanzade <hjavad522@gmail.com>
2022-03-31 20:36:58 +0430

    feat: create edit ingredient page and add functionallities

---
+++ pages/ingredients/[id].tsx
@@ -0,0 +1,106 @@
+import Button from "@/atoms/button";
+import Check from "@/atoms/check";
+import Input from "@/atoms/input";
+import Title from "@/atoms/title";
+import Seo from "@/molecules/seo";
+import Layout from "@/organisms/layout";
+import { getAxiosInstanse } from "api/api";
+import type { GetServerSideProps, NextPage } from "next";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+interface IEditIngredient {
+  ingredient: IIngredient;
+  id: number;
+}
+
+const EditIngredient: NextPage<IEditIngredient> = ({ ingredient, id }) => {
+  const [name, setName] = useState(ingredient.name);
+  const [quantity, setQuantity] = useState(ingredient.quantity);
+  const [price, setPrice] = useState(ingredient.price);
+  const [isComplete, setIsComplete] = useState(ingredient.isComplete);
+  const router = useRouter();
+
+  const handleForm = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const ingredient = {
+      name: name,
+      quantity: quantity,
+      price: price,
+      isComplete: isComplete,
+    };
+
+    await getAxiosInstanse()
+      .put("ingredients/" + id, ingredient)
+      .then((res) => router.push("/ingredients"))
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <>
+      <Seo title="Ingredients"></Seo>
+      <Layout translations={""}>
+        <Title>Add New Ingredient</Title>
+        <FormIng className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <Input
+                name="name"
+                placeHolder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="col-md-12">
+              <Input
+                name="quantity"
+                placeHolder="Quantity"
+                value={quantity}
+                onChange={(e) => setQuantity(e.target.value)}
+              />
+            </div>
+            <div className="col-md-12">
+              <Input
+                name="price"
+                placeHolder="Price"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              />
+            </div>
+            <div className="col-md-12 text-center">
+              <Check
+                className="my-2"
+                label="Is Ingredient Complete"
+                name="isComplete"
+                checked={isComplete}
+                onChange={() => setIsComplete(!isComplete)}
+              />
+            </div>
+            <div className="col-12 text-center">
+              <Button onClick={(e) => handleForm(e)}>Save</Button>
+            </div>
+          </div>
+        </FormIng>
+      </Layout>
+    </>
+  );
+};
+
+export default EditIngredient;
+
+const FormIng = styled.form`
+  max-width: 50%;
+`;
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const ingredientRes = await fetch(
+    process.env.NEXT_PUBLIC_API_URL + "ingredients/" + ctx.params.id ||
+      "http://localhost:5000"
+  );
+  const ingredient = await ingredientRes.json();
+
+  return {
+    props: { ingredient, id: ctx.params.id }, // will be passed to the page component as props
+  };
+};
