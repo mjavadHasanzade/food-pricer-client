7788fd93ddaf2cf2c49ceb2a3d0419d611e1cc39
MJavad Hasanzade <hjavad522@gmail.com>
2022-04-15 13:54:33 +0430

    feat: add context api to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ToastProvider } from "react-toast-notifications";
+import AppProvider from "context/app-context";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ToastProvider autoDismiss>
-      <Component {...pageProps} />
+      <AppProvider>
+        <Component {...pageProps} />
+      </AppProvider>
     </ToastProvider>
   );
 }
