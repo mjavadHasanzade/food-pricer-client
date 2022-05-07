87f77e9b016d97f526a76150454e39099def9677
MJavad Hasanzade <hjavad522@gmail.com>
2022-05-07 23:23:18 +0430

    feat: create get cookie fuction

--- utils/setCookie.ts
+++ utils/setCookie.ts
@@ -9,4 +9,18 @@ export const setCookie = (
   document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
 };
 
-
+export const getCookie = (cookieName: string) => {
+  let name = cookieName + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+};
